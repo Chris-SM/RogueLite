ec90c3cef8bf934205a2ae2b0bf420f7072cea1b
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2025-11-19 17:03:49 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'roguelikeApp',
+  webDir: 'www'
+};
+
+export default config;
